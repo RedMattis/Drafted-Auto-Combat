2049b760b1cca9c823bd2e2a3f2659fb2b172392
RedMattis <redmattis@gmail.com>
2025-08-10 18:32:47 +0200

    first commit

---
+++ NotBigAndSmall/Source/Drafted Auto-Combat/Main.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using Verse;
+using HarmonyLib;
+//using VariedBodySizes;
+
+namespace BigAndSmall
+{
+    [StaticConstructorOnStartup]
+    public static class AutoCombat
+    {
+        private static readonly Type patchType;
+        public static Harmony harmony = new("RedMattis.AutoCombat");
+        static AutoCombat()
+        {
+            patchType = typeof(AutoCombat);
+            harmony.PatchAll();
+        }
+    }
+}
